9361cbe74816c38f344892292c5167088a4c0ba1
Juan Soledad <30356956+charlysoledad@users.noreply.github.com>
2021-04-25 00:10:45 -0500

    Index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ProjectComponent } from './projects/projects.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'projects', component: ProjectComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''},
+  {path: '**', pathMatch: 'full', redirectTo: '', component: AppComponent},
 ];
 
 @NgModule({
